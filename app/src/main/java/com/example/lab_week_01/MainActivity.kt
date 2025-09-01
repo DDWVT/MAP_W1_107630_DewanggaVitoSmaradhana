57a1732aaca319ab6843712d1150414da8f02012
DDWVT <dewa.vito2005@gmail.com>
2025-09-01 14:10:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_01/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.lab_week_01
+
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val nameDisplay = findViewById<TextView>(R.id.name_display)
+        val nameSubmit = findViewById<Button>(R.id.name_submit)
+
+        nameSubmit.setOnClickListener {
+            val nameInput = findViewById<TextInputEditText>(R.id.name_input)
+                ?.text.toString().trim()
+            val nimInput = findViewById<TextInputEditText>(R.id.nim_input)
+                ?.text.toString().trim()
+
+            if (nameInput.isEmpty()) {
+                Toast.makeText(this, getString(R.string.name_empty), Toast.LENGTH_LONG).show()
+            } else if (nimInput.length != 11) {
+                Toast.makeText(this, getString(R.string.student_number_invalid), Toast.LENGTH_LONG).show()
+            } else {
+                nameDisplay.text = getString(R.string.name_greet) +
+                        " $nameInput\nNIM: $nimInput"
+            }
+        }
+    }
+}
+
